db07e5fa9c1cf34901260c2d5a341610182b92b2
anuj <anuj.sharma@enercare.ca>
2017-11-15 15:32:56 -0500

    test

--- Realestate/index.js
+++ Realestate/index.js
@@ -4,6 +4,25 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
